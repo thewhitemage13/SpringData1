15d52725f4de8560b6b3dc52799f0d2bdb2d9e67
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-07-07 20:29:15 +0200

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import com.example.demo.repository.PersonRepository;
+import entity.Person;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.NoSuchElementException;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.demo.repository")
+@EntityScan(basePackages = "entity") // Указываем корректный пакет для сканирования сущностей
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(PersonRepository repository) {
+		return args -> {
+			Person person = Person.builder()
+					.name("Katye")
+					.surname("Vyacheslavovna")
+					.lastName("efweFWEF")
+					.build();
+
+//			repository.save(person);
+
+			for (Person person1 : repository.findAll()) {
+				System.out.println(repository.findAll());
+			}
+		};
+	}
+
+}
